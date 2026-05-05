dc257f9604c7a9d2bc8b0f2802654fd08c7a6302
harshkatke <harshkatke14@gmail.com>
2026-05-05 20:56:00 +0530

    Add files via upload

---
+++ FirstCode python.py
@@ -0,0 +1,17 @@
+print("Hello world")
+
+tuple={1,2,1,3}
+print(tuple)
+
+list=[1,2,2]
+print(list)
+
+dict={"key":1,"roll":1}
+print(dict)
+
+set={1,1,2,3}
+print(set)
+
+a=11
+print(a)
+id=id(int(a))
